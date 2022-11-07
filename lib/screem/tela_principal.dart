89a6b145ae8b4f9aa1d834521c85ccac67eb98b8
Maveryk <pedrodutra1r@gmail.com>
2022-11-07 19:28:12 -0300

    @

--- lib/screem/tela_principal.dart
+++ lib/screem/tela_principal.dart
@@ -7,7 +7,8 @@ class TelaPrincipal extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Trip With IFF")),
+      appBar:
+          AppBar(title: const Text("Trip With IFF"), actions: [SwitchAppBar()]),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
@@ -25,7 +26,7 @@ class TelaPrincipal extends StatelessWidget {
               ),
             ),
             Padding(
-              padding: const EdgeInsets.all(10.0),
+              padding: const EdgeInsets.all(8.0),
               child: Image.asset("Image/viagem.jpg", width: 500, height: 300),
             ),
             const Text(
@@ -62,6 +63,32 @@ class TelaPrincipal extends StatelessWidget {
           ],
         ),
       ),
+    );
+  }
+}
+
+class SwitchAppBar extends StatefulWidget {
+  const SwitchAppBar({super.key});
+
+  @override
+  State<SwitchAppBar> createState() => _SwitchAppBarState();
+}
+
+class _SwitchAppBarState extends State<SwitchAppBar> {
+  bool light = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Switch(
+      // This bool value toggles the switch.
+      value: light,
+      activeColor: Colors.white,
+      onChanged: (bool value) {
+        // This is called when the user toggles the switch.
+        setState(() {
+          light = value;
+        });
+      },
     );
   }
 }
